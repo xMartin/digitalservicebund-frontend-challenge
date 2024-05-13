82020414d57de5f659534969fa7940d0647c9cfb
Martin Stadler <martin.stadler@xmmail.de>
2024-05-13 16:43:11 +0200

    Improve display of datasets table including percentage bars

--- src/Datasets.test.tsx
+++ src/Datasets.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Datasets from "./Datasets";
+import Datasets, { ApiDataEntry } from "./Datasets";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 it("renders heading", () => {
@@ -10,4 +10,45 @@ it("renders heading", () => {
   );
 
   expect(screen.getByText(/Datasets by Ministry/)).toBeInTheDocument();
+});
+
+it("renders table sorted by number of datasets, with percentage bars", async () => {
+  const payload: ApiDataEntry[] = [
+    {
+      department: "X",
+      description: "",
+      datasets: 2,
+    },
+    {
+      department: "Z",
+      description: "",
+      datasets: 80,
+    },
+    {
+      department: "Y",
+      description: "",
+      datasets: 8,
+    },
+  ];
+
+  window.fetch = vi.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(payload),
+    } as Response),
+  );
+
+  render(
+    <QueryClientProvider client={new QueryClient()}>
+      <Datasets />
+    </QueryClientProvider>,
+  );
+
+  const topValue = (await screen.findAllByRole("cell"))[1].innerHTML;
+  expect(topValue).toBe("80");
+
+  const secondRowCell = (await screen.findAllByRole("cell"))[2];
+  const innerBarSpan = secondRowCell.firstElementChild?.firstElementChild;
+  // eslint-disable-next-line
+  const width = (innerBarSpan as any)?.style.width;
+  expect(width).toBe("10%");
 });
--- src/Datasets.tsx
+++ src/Datasets.tsx
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import Card from "./Card";
+import styles from "./Datasets.module.css";
 
-type Dataset = {
+export type ApiDataEntry = {
   department: string;
   description: string;
   datasets: number;
+};
+
+type AppDataEntry = ApiDataEntry & {
+  percent: number;
 };
 
 export default function Datasets() {
@@ -12,22 +17,53 @@ export default function Datasets() {
     queryKey: ["datasets"],
     queryFn: async () => {
       const result = await fetch(import.meta.env.VITE_DATA_URL as string);
-      return (await result.json()) as Dataset[];
+      return (await result.json()) as ApiDataEntry[];
+    },
+    select: (apiData) => {
+      let max = 0;
+      for (const apiDataEntry of apiData) {
+        if (apiDataEntry.datasets > max) {
+          max = apiDataEntry.datasets;
+        }
+      }
+
+      const result: AppDataEntry[] = [];
+      for (const apiDataEntry of apiData) {
+        result.push({
+          ...apiDataEntry,
+          percent: (apiDataEntry.datasets / max) * 100,
+        });
+      }
+
+      result.sort((a, b) => {
+        return a.datasets > b.datasets ? -1 : a.datasets === b.datasets ? 0 : 1;
+      });
+
+      return result;
     },
   });
 
   return (
     <Card title="Datasets by Ministry">
-      <table>
-        <tbody>
-          {(data || []).map((entry) => (
-            <tr key={entry.department}>
-              <th>{entry.department}</th>
-              <td>{entry.datasets}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {data && (
+        <table className={styles.table}>
+          <tbody>
+            {data.map((entry, index) => (
+              <tr key={entry.department} className={styles.row}>
+                <td className={styles.department}>
+                  {entry.department}
+                  <span className={styles.bar}>
+                    <span style={{ width: `${entry.percent}%` }}></span>
+                  </span>
+                </td>
+                <td id={index.toString()} className={styles.value}>
+                  {entry.datasets}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </Card>
   );
 }
